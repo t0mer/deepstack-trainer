00256c5cb68ad50edf5a8d33bd373fdb6a703688
T0mer <tomer.klein@gmail.com>
2021-07-22 20:58:32 +0300

    adding gallery

--- trainer/dist/js/home.js
+++ trainer/dist/js/home.js
@@ -5,6 +5,10 @@ $(document).on('change', '.btn_file :file', function () {
 });
 
 
+
+
+
+
 $('.btn_file :file').on('fileselect', function (event, label) {
     var input = $(this).parents('.input-group').find(':text'),
         log = label;
@@ -27,12 +31,10 @@ function readURL(input, id) {
 }
 
 
-function Validate(pairs)
-{
-    for(var pair of pairs) {
-        if(pair[0]=='teach_file' || pair[0]=='who_file' || pair[0]=='detect_file' || pair[0]=='scene_file') 
-        {
-            if(pair[1].name==""){
+function Validate(pairs) {
+    for (var pair of pairs) {
+        if (pair[0] == 'teach_file' || pair[0] == 'who_file' || pair[0] == 'detect_file' || pair[0] == 'scene_file') {
+            if (pair[1].name == "") {
                 ShowError("No image is selected, Please select image and try again.");
                 return false;
             }
@@ -43,20 +45,18 @@ function Validate(pairs)
                 return false;
             }
         }
-        if(pair[0]=='person')
-        {
-            if(pair[1]===""){
+        if (pair[0] == 'person') {
+            if (pair[1] === "") {
                 ShowError("Please enter person name");
                 return false;
             }
         }
-      
-     }
+
+    }
     return true;
 }
 
-function ShowError(message)
-{
+function ShowError(message) {
     Swal.fire({
         title: 'Error!',
         text: message,
@@ -66,7 +66,29 @@ function ShowError(message)
 }
 
 
+function load_gallery() {
+    $.ajax(
+        {
+            url: '/api/images',
+            success: function (data) {
+
+                $('#gallery').html(data)
+
+
+            },
+            error: function (e) {
+
+            }
+        });
+}
+
+
 $(document).ready(function () {
+
+
+    $('#gallerytab').click(function(){
+        load_gallery();
+    });
 
     $(".imgfile").change(function () {
         var action = $(this).attr('id').split("_")[0];
@@ -74,13 +96,17 @@ $(document).ready(function () {
     });
 
 
+
+
+
+
     $('.btn-post').click(function () {
         var action = $(this).attr('id').split("-")[0];
         var form_data = new FormData($('#' + action + '-form')[0]);
-        if(!Validate(form_data.entries())){
+        if (!Validate(form_data.entries())) {
             return;
         }
-    
+
         $.ajax({
             type: 'POST',
             url: '/' + action,
